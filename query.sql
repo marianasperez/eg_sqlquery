6ae2e2244745a06480f97169f6144bd4dbf19ecf
Mari Perez <62081610+marianasperez@users.noreply.github.com>
2024-12-19 01:18:43 +1100

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,5 +1,5 @@
-SELECT 
-    id, user_id, game_id, value, multiplier, profit, created_at, status, active, data_origin
+SELECT DISTINCT 
+    id, user_id, game_id, value, multiplier, profit, created_at, status, active
 FROM (
     -- Bets from the inactive_bets table
     SELECT 
@@ -8,22 +8,22 @@ FROM (
     FROM inactive_bets
     WHERE DATE(created_at) = CURDATE() - INTERVAL 1 DAY
 
-    UNION ALL
+    UNION
 
     -- Bets from the active_sportsbook_bet table (settled bets only)
     SELECT 
-       id, user_id, game_id, value, multiplier, profit, created_at, status, active,
+        id, user_id, game_id, value, multiplier, profit, created_at, status, active,
         'active_sportsbook_bet' AS data_origin
     FROM active_sportsbook_bet
     WHERE DATE(created_at) = CURDATE() - INTERVAL 1 DAY
       AND status = 'settled'
       AND active = false
 
-    UNION ALL
+    UNION
 
     -- Bets from the inactive_sportsbook_bet table
     SELECT 
-         id, user_id, game_id, value, multiplier, profit, created_at, status, active,
+        id, user_id, game_id, value, multiplier, profit, created_at, status, active,
         'inactive_sportsbook_bet' AS data_origin
     FROM inactive_sportsbook_bet
     WHERE DATE(created_at) = CURDATE() - INTERVAL 1 DAY
